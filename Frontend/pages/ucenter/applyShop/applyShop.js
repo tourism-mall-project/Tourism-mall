87d301c536c80439db8205d2fd5a452f81386ae7
zjj-aee <1804048367@qq.com>
2019-03-07 16:49:48 +0800

    litemall-admin

--- Frontend/pages/ucenter/applyShop/applyShop.js
+++ Frontend/pages/ucenter/applyShop/applyShop.js
@@ -116,7 +116,6 @@ Page({
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    this.nowlocation();
   },
   onReady: function () {
 
---
+++ litemall-admin/config/dev.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+	NODE_ENV: '"development"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"http://122.152.206.172:8080/admin"'
+}
